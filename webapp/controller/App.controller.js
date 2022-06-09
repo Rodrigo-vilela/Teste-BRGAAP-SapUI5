512dccf3c0d4689ada7a0e0b7c4dffbc3ed82447
Rodrigo-vilela <rodrigo.vilela2@hotmail.com>
2022-06-08 22:08:49 -0300

    Commit teste alteração

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller"
  ], function (Controller) {
     "use strict";
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", { 
         onShowHello : function () {
            // show a native JavaScript alert
            alert("Hello World");
